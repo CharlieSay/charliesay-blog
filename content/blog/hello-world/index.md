7ce75c7fa3bdbb1495db0183a8ba799b6ea8ec73
CharlieSay <charlie.w.say@live.co.uk>
2021-06-09 19:40:36 +0100

    Charlie Say blog base

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -1,5 +1,5 @@
 ---
-title: Hello World
+title: Impostor Syndrome and its affect on junior developers
 date: "2015-05-01T22:12:03.284Z"
 description: "Hello World"
 ---
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Charlie Say Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Charlie Say`,
+      summary: `lives in MCR, UK and loves all things full stack.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://www.charliesay.xyz`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `charliesay_`,
     },
   },
   plugins: [
